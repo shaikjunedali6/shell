b68d305bc084c99402b6d5ad2348900235b4b079
iam-junedali <junedali.sk@gmail.com>
2024-11-14 20:44:06 +0530

    commit

--- first-shellscript.sh
+++ first-shellscript.sh
@@ -3,10 +3,19 @@
 #Author = Juned Ali Shaik
 #Date = 11/14/2024.
 
+#
 echo "This is my first shell script"
 
+#
+ls
 
-ls -ltr
+#
 pwd
 
-sudo apt update -y
+#sudo apt update -y
+
+#Create the directory
+mkdir junedali
+
+Delete the directory.
+sudo rm -f junedali
