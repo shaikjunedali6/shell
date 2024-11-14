6065933729a1924c5b1f029b3a67409203313212
iam-junedali <junedali.sk@gmail.com>
2024-11-14 20:58:08 +0530

    commit

--- first-shellscript.sh
+++ first-shellscript.sh
@@ -3,7 +3,8 @@
 #Author = Juned Ali Shaik
 #Date = 11/14/2024.
 
-#
+Set -X
+
 echo "This is my first shell script"
 
 #
---
+++ realtime-project.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#Author - Juned Ali Shaik.
+#Date - 11/14/2024.
+
+#Create a bash script to check the node health
+
